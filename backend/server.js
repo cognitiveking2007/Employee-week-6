2d7774a4b336607843aa9799d74e0b46787f3bc1
cognitiveking2007 <ratakondalalith@gmail.com>
2026-04-18 19:36:48 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,12 @@ app.use(
 app.use(exp.json());
 app.use("/EmployeeAPI", EmployeeApp);
 //asign port
-const port=process.env.PORT ||4000
+//const port=process.env.PORT ||4000
 const startServer = async () => {
   try {
     // 3. Use mongoose.connect, NOT the function name connectDB
-    const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/Employee";
+    //const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/Employee";
+    const dbUrl = process.env.DB_URL;
     await mongoose.connect(dbUrl);
     console.log("DB server connected");
 
